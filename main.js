294336dd0bd8e61b052a66c58e7e006f29cbb142
刘卫伟 <326634135@qq.com>
2024-03-27 21:57:21 +0800

    commit

--- main.js
+++ main.js
@@ -4,23 +4,22 @@ import App from './App'
 import Vue from 'vue'
 import './uni.promisify.adaptor'
 import uView from '@/uni_modules/uview-ui'
+
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-	...App
+  ...App
 })
 Vue.use(uView)
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import {
-	createSSRApp
-} from 'vue'
+import { createSSRApp } from 'vue'
 export function createApp() {
-	const app = createSSRApp(App)
-	return {
-		app
-	}
+  const app = createSSRApp(App)
+  return {
+    app
+  }
 }
 // #endif
